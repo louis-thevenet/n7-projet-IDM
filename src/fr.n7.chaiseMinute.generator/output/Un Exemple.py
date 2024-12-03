317a80a799dd7363ee6c64bdda3f7455e1f04a66
louis thevenet <louis.tvnt@gmail.com>
2024-12-03 13:10:13 +0100

    feat: working imported column and data columns

--- src/fr.n7.chaiseMinute.generator/output/Un Exemple.py
+++ src/fr.n7.chaiseMinute.generator/output/Un Exemple.py
@@ -1,10 +1,33 @@
 import sys
+import pandas as pd
 
 def main():
-	filename = sys.argv[1]
-	print(filename)
-	f = open(filename, "r")
-	print(f.read()) 
+# Load files
+	content = {}	
+	csv_file_path = "table1.csv"
+	content["table1"] = pd.read_csv(csv_file_path)
+	csv_file_path = "table2.csv"
+	content["table2"] = pd.read_csv(csv_file_path)
+
+	out = {}
+
+	out["A"]=content["table1"]["A"]
+
+	out["B"]=content["table1"]["B"]
+
+	pd.DataFrame.from_dict(out, orient="columns").to_csv("output_"+"table1"+".csv", index_label="index")
+	out = {}
+
+	out["C"]=content["table2"]["C"]
+
+	out["B"]=content["table2"]["B"]
+
+	# Looking for table1.B 
+	out["A"]=content["table1"]["B"]
+
+		
+	pd.DataFrame.from_dict(out, orient="columns").to_csv("output_"+"table2"+".csv", index_label="index2")
 	
 if __name__ == '__main__':
 	main()
+
